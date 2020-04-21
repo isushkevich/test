7e63f430304d43f56d122ca2cb9f931e4c19b21d
isushkevich <ivan.sushik@gmail.com>
2020-04-21 18:48:29 +0300

    create pic gallery

---
+++ js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    centerMode: true,
+    centerPadding: '0px',
+    // responsive: [{
+    //         breakpoint: 1200,
+    //         settings: {
+    //             slidesToShow: 2,
+    //             slidesToScroll: 2
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 768,
+    //         settings: {
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1
+    //         }
+    //     }
+    // ]
+});
